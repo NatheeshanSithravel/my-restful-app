f111721897f6b39bebf12d734f45ca8a99ee4d69
Natheeshan Sithravel <natheeshan.20244025@iit.ac.lk>
2025-03-25 09:03:28 +0530

    Add files via upload

--- src/main/java/payroll/PayrollApplication.java
+++ src/main/java/payroll/PayrollApplication.java
@@ -2,11 +2,18 @@ package payroll;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class PayrollApplication {
+public class PayrollApplication extends SpringBootServletInitializer {
 
-	public static void main(String... args) {
-		SpringApplication.run(PayrollApplication.class, args);
-	}
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(PayrollApplication.class);
+    }
+
+    public static void main(String... args) {
+        SpringApplication.run(PayrollApplication.class, args);
+    }
 }
